996a6371ae6e11541d40d481a4780cb16837f9ac
yeiler2111 <yeiler2209@gmail.com>
2024-07-13 14:59:32 -0500

    solo el dist

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createPinia } from "pinia";
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router/index";
-
-const pinia = createPinia();
-
-createApp(App).use(router).use(pinia).mount("#app");
-
